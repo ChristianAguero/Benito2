adb7ae1ef24728071d4043153334513a9b6e6ca1
ChristianAguero <christian.aguero.aguero@potros.itson.edu.mx>
2023-03-21 10:53:07 -0700

    Completando la persistencia

--- src/mx/itson/benito/persistencia/ProveedorDAO.java
+++ src/mx/itson/benito/persistencia/ProveedorDAO.java
@@ -5,10 +5,167 @@
  */
 package mx.itson.benito.persistencia;
 
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.swing.JOptionPane;
+import mx.itson.benito.entidades.*;
+import mx.itson.benito.utilerias.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
 /**
  *
  * @author lahg2
  */
 public class ProveedorDAO {
+    
+    /**
+     * 
+     * @return 
+     */
+    public List<Proveedor> ObtenerTodos(){
+    
+        List<Proveedor> proveedores = new ArrayList<>();
+
+        try{
+            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            
+            CriteriaQuery<Proveedor> criteriaQuery = session.getCriteriaBuilder().createQuery(Proveedor.class);
+            criteriaQuery.from(Proveedor.class);
+            
+            proveedores = session.createQuery(criteriaQuery).getResultList();
+                    
+        }catch(Exception ex){
+            
+            System.err.println("Ocurrio un error" + ex);
+            
+        }
+        
+        return proveedores;
+    
+    }
+    
+    public boolean guardar(String nombre, String clave, String telefono, String contacto){
+        
+        boolean resultado = false;
+        
+        try{
+            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            
+            Proveedor p = new Proveedor();
+            p.setNombre(nombre);
+            p.setClave(clave);
+            p.setTelefono(telefono);
+            p.setContacto(contacto);
+            
+            session.save(p);
+            
+            session.getTransaction().commit();
+            
+            resultado = p.getId() != 0;
+            
+        }catch(Exception ex){
+            
+                System.err.println("Ocurrio un error" + ex.getMessage());
+            
+        }
+        
+        return resultado;
+        
+    }
+    
+    public Proveedor obtenerPorId(int id){
+        
+        Proveedor proveedor = new Proveedor();
+        
+        try{
+            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            proveedor = session.get(Proveedor.class,  id);
+            
+        }catch(HibernateException ex){
+            
+            System.err.println("Ocurrio un error: " + ex);
+            
+        }
+        
+        return proveedor;
+        
+    }
+    
+    public boolean editar(int id, String nombre, String numeroLicencia, Date fechaAlta){
+        
+        boolean resultado = false;
+        
+        try{
+            
+            Session session = HibernateUtil.getSessionFactory().openSession();
+            session.beginTransaction();
+            
+            Conductor conductor = obtenerPorId(id);
+            
+            if(conductor != null){
+                
+                conductor.setNombre(nombre);
+                conductor.setFechaAlta(fechaAlta);
+                conductor.setNumeroLicencia(numeroLicencia);
+                
+                session.saveOrUpdate(conductor);
+                session.getTransaction().commit();
+                
+                resultado = true;
+                
+            }
+            
+        }catch(HibernateException ex){
+            
+            System.err.println("Ocurrio un error: " + ex);
+            
+        }
+        
+        return resultado;
+        
+    }
+    
+    public boolean eliminar(int id){
+        
+        boolean resultado = false;
+        
+        String[] options = {"Si", "No"};
+        int x = JOptionPane.showOptionDialog(null, "El registro sera eliminado ¿Esta seguro de esto?", "Atencion", JOptionPane.DEFAULT_OPTION, JOptionPane.WARNING_MESSAGE, null, options, options[0]);
+        
+        if(x == 0){
+        
+            try{
+
+               Session session = HibernateUtil.getSessionFactory().openSession();
+               session.beginTransaction();
+
+               Conductor conductor = obtenerPorId(id);
+
+               if(conductor != null){
+
+                   session.delete(conductor);
+                   session.getTransaction().commit();
+
+                   resultado = true;
+
+               }
+
+            }catch(HibernateException ex){
+
+               System.err.println("Ocurrio un error: " + ex);
+
+           }
+            
+        }
+         
+         return resultado;
+        
+    }
     
 }
